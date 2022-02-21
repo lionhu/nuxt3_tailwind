30195eb16aa5e2af6cec4265a699f6f6fb046bf4
HaiguangHU <huhaiguang@me.com>
2022-02-21 10:17:01 +0900

    3D button

--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,11 @@
 <template>
-<div class="body">
-  <WidgetsRollingCards></WidgetsRollingCards>
-</div>
+  <div class="min-h-screen p-4 bg-blue-500 lg:p-10">
+    <!-- <WidgetsRollingCards></WidgetsRollingCards> -->
+    <div class="flex items-center justify-center ">
+      <button class="relative text-xl font-thin">
+        <div class="absolute inset-x-0 h-full border rounded-lg bg-grey-100 border-grey-100 -bottom-1"></div>
+        <div class="relative px-10 py-4 transition duration-500 transform bg-gray-100 border border-gray-500 rounded-lg hover:translate-y-1">click Me</div>
+      </button>
+    </div>
+  </div>
 </template>
