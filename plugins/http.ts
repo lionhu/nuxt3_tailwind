e3657fb6750ca0fc7282f5b25597d612080a08ac
HaiguangHU <huhaiguang@me.com>
2022-04-12 22:49:38 +0900

    change settings of vscode

--- plugins/http.ts
+++ plugins/http.ts
@@ -1,20 +1,21 @@
 import { defineNuxtPlugin } from '#app'
 import axios from 'axios'
 
-const access_token = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ3Nzc0Mzg1LCJqdGkiOiJhNWI0ZGM1MzIwOTM0ZjFmOGJjOGY2ZDIwNDFlY2NiOCIsInVzZXJfaWQiOjV9.ipO8JRzqAuKYu-OEMLoMv1lsok5kB6Vk37wRhsNuSZg`;
-const _timeout=5000
+const access_token = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUwNjM1MjM4LCJqdGkiOiI0YzMzNjBjY2YyZDM0NmQzYjI4ODNlZTdkNTk4NTc5OSIsInVzZXJfaWQiOjF9.HtmwGcJtCL9JS4LOobEGY76nds8iD9x-m8c7xD2zY3o`
+const _timeout = 5000
 const instance = axios.create({
-  baseURL: "https://misawa-pharmacy.nichiei.services/daphne/api",
+  baseURL: 'https://misawa-pharmacy.nichiei.services/daphne/api',
   headers: {
-      "Content-Type": "application/json",
+    'Content-Type': 'application/json',
     //   "lionhu":access_token
-      "Authorization": access_token
+    Authorization: access_token
   },
-  timeout: _timeout,
-});
+  timeout: _timeout
+})
 
-console.log(instance);
-export default defineNuxtPlugin(nuxtApp => {
+console.log('http instance:', instance)
+
+export default defineNuxtPlugin((nuxtApp) => {
   return {
     provide: {
       http: instance
